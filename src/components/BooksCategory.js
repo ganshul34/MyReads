ffd009be4c936b18dbcc945e84161aec72794263
ganshul34 <ganshul34@gmail.com>
2018-09-07 15:09:38 +0530

    Added initial code and lot to come

---
+++ src/components/BooksCategory.js
@@ -0,0 +1,12 @@
+import  React from 'react'
+import Proptypes from 'prop-types'
+import { Link } from 'react-router-dom'
+
+export default class BooksCategory extends React.Component{
+    static proptypes ={
+        books: Proptypes.array,
+        bookTitle: Proptypes.string.isRequired,
+        onChange: Proptypes.func.isRequired
+    } 
+    
+}
